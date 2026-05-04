f92fe4fef4369dc5ffe7472366c58b619122a243
darksr789 <dyt27380@gmail.com>
2026-05-04 12:49:08 +0530

    config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  BOT_NAME: 'WOLF TECH',
-  SESSION_PREFIX: 'WOLF',
-  AUTHOR: 'Silent Wolf',
-  DEPLOY_HOST: 'wolfxpair.xwolf.space',
-  PANEL_HOST: 'wolfxpair.xwolf.space',
-  GITHUB_URL: 'https://github.com/wolfxpair/wolfxpair',
+  BOT_NAME: 'SURYA-X',
+  SESSION_PREFIX: '.',
+  AUTHOR: 'DARK SURYA',
+  WA_CHANNEL: 'https://whatsapp.com/channel/0029Vb64JNKJf05UHKREBM1h',
+  WA_GROUP: 'https://chat.whatsapp.com/L0oWvAe4eeb6HBYIEPXGbo?mode=gi_t',
+  GITHUB_URL: 'https://github.com/darksurya345/SURYA-X',
   PORT: process.env.PORT || 3000,
 };
